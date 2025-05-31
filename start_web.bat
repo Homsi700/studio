1231474f705a52ab8923a0e9ad014053a0756161
Nabil Homsi <sezarooo44@gmail.com>
2025-05-31 13:04:19 +0000

    طيب حدثي سكربت تشغيل ليفتح متصفح تلقائيا لو سمحتي

--- start_web.bat
+++ start_web.bat
@@ -3,15 +3,18 @@ echo Building Dawami web application for production...
 npm run build
 echo.
 echo Starting Dawami web application...
-rem Consider adding -H 0.0.0.0 if npm start doesn't listen on all interfaces by default
-npm start
+npm start & (
+    echo Waiting for application to start...
+    timeout /t 5 /nobreak > nul
+    echo Opening Dawami in your default browser...
+    start http://localhost:3000
+)
 echo.
 echo Dawami is now running.
 echo You can access it via your web browser, typically at:
 echo   - http://localhost:3000 (from the local computer)
 echo   - http://[Your_Computer_Local_IP]:3000 (from other devices on the same local network)
 echo.
-echo Make sure your firewall allows connections to port 3000 if accessing from other devices.
-echo.
-echo Press Ctrl+C in this window to stop the application.
+echo This window will close automatically if the application stops.
+echo To stop the application manually, close this window or press Ctrl+C in the terminal where "npm start" is running.
 pause
